48a44fa86aa7189b0a5c8dceb7487f8892a59906
brnpessoa14 <79914261+brnpessoa14@users.noreply.github.com>
2025-04-03 23:30:36 -0300

    Create hta.sql

---
+++ hta.sql
@@ -0,0 +1,23 @@
+@startmindmap Cansat
+* Site
+**[#Blue] Interface
+***[#LightBlue] Simples e Objetiva
+***[#LightBlue] Página Inicial Clara, Atraente e Fluida
+**[#Green] Conteúdo
+***[#LightGreen] Informações sobre o que é um Cansat
+****[#LightGreen] Exemplos de Cansat
+***[#LightGreen] Detalhes do Evento
+****[#LightGreen] Hora
+****[#LightGreen] Local
+****[#LightGreen] Data
+**[#Red] Funcionalidades
+***[#Orange] Formulário de Inscrição Visível e Claro
+***[#Orange] Aba de Contato
+****[#Orange] Dúvidas e Suporte
+**[#Purple] Informações Institucionais
+***[#Pink] Colaboradores do Evento
+***[#Pink] Organizadores e seus Prestígios
+**[#DarkGray] Recursos Adicionais
+***[#Gray] Links para Artigos e Vídeos
+****[#Gray] Imagens e Descrições dos Projetos Premiados
+@endmindmap
